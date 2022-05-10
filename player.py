bb556acd231c8100ac66efdde78d4e7ba880eb99
otieno-juma <103896399+otieno-juma@users.noreply.github.com>
2022-05-10 21:48:16 +0300

    Update player.py
    paul's update

--- player.py
+++ player.py
@@ -8,6 +8,15 @@ class Player:
         self.game_in_progress = True
         self.initial_score = 300
         self.total_score = 300
+
+    def start_game(self):
+
+        while self.game_in_progress:
+
+            self.obtain_inputs()
+            self.obtain_updates()
+            self.obtain_outputs()
+
         
     def obtain_inputs(self):
         guess = input('Higher or Lower: [H/L] ').upper()
@@ -15,3 +24,9 @@ class Player:
         
         play_game = input('Play again? [Y/N] ').upper()
         self.game_in_progress = (play_game == 'Y')
+
+    def obtain_updates(self):
+
+         if not self.game_in_progress:
+            return 
+        
