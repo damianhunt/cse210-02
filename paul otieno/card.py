dcd48a12fd704dcede32c657a40409c5ce4473fe
otieno-juma <103896399+otieno-juma@users.noreply.github.com>
2022-05-12 19:45:05 +0300

    paul otieno

---
+++ paul otieno/card.py
@@ -0,0 +1,56 @@
+from player import Player
+    
+
+class Player:
+
+    def __init__(self):
+        """
+         Contructs a new player.
+    
+        Args:
+        self (player): an instance of player.
+         """
+        self.game_in_progress = True
+        self.player = Player()
+        
+    def input(self):
+
+        self.player.get_card_number()
+
+    def start_game(self):
+        """Starts the game by running the main game loop.
+        
+        Args:
+            self (player): an instance of player.
+        """
+
+        while self.game_in_progress:
+
+            self.updates()
+            self.outputs()
+            self.input()
+            self.keep()
+
+    def updates(self):
+        """Updates the player's score.
+        
+        Args:
+            self (player): an instance of player.
+        """
+        return self.player.points_earned()
+        
+    def out_puts(self):
+        """Displays the score. Also asks the player if they want to roll again. 
+
+        Args:
+            self (player): An instance of player.
+        """
+        self.player.get_card_number()
+        print(f"The card is {self.player.card_number}")
+
+    def keep(self):
+        keep = input("keep playing? [y/n]")
+        if keep == "y":
+            self.game_in_progress = True
+        elif keep =="n":
+            self.game_in_progress = False
---
+++ paul otieno/player.py
@@ -0,0 +1,28 @@
+import random
+
+class Card:
+
+    def __init__(self):
+        
+        self.card_number = 0
+        self.player = 0
+        self.score = 300
+
+    def get_card_number(self):
+        self.card_number = random.randint(1, 13)
+    
+    def points_earned(self):
+        self.get_card_number()
+        guess_card = input("Select High or Lower [H/L]: ")
+        second_number = random.randint(1, 13)
+        if second_number >= self.card_number and guess_card == "H":
+            self.score += 100
+        elif second_number <= self.card_number and guess_card == "H":
+            self.score -= 70
+        elif second_number >= self.card_number and guess_card == "L":
+            self.score -= 75
+        elif second_number <= self.card_number and guess_card == "L":
+            self.score += 100
+        print(f"The next card was: {second_number}")
+        print(f"Your acore is: {self.score}")        
+        
